2e988de36a31de4490f39f9baacdae1cd9152166
metaflie <105317230+metaflie@users.noreply.github.com>
2023-12-29 13:51:50 +0100

    Add files via upload

---
+++ game_logic.js
@@ -0,0 +1,88 @@
+let playerText = document.getElementById('playerText');
+let restartBtn = document.getElementById('restartBtn');
+let boxes = Array.from(document.getElementsByClassName('box'));
+
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks');
+
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
+let gameOver = false; // Variable to track if the game is over
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked));
+}
+
+function boxClicked(e) {
+    if (gameOver) {
+        return; // Do nothing if the game is already over
+    }
+
+    const id = e.target.id;
+
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        if (playerHasWon() !== false) {
+            playerText.innerHTML = `${currentPlayer} has won!`;
+            let winning_blocks = playerHasWon();
+
+            winning_blocks.map(box => boxes[box].style.backgroundColor = winnerIndicator);
+
+            // Update result table
+            let resultTable = document.getElementById("resultTable");
+            let row = resultTable.insertRow(-1);
+            let playerCell = row.insertCell(0);
+            let resultCell = row.insertCell(1);
+            playerCell.innerHTML = currentPlayer;
+            resultCell.innerHTML = "Won";
+
+            gameOver = true; // Set the game to be over
+            return;
+        }
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+    }
+}
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition;
+
+        if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a, b, c];
+        }
+    }
+    return false;
+}
+
+restartBtn.addEventListener('click', restart);
+
+function restart() {
+    spaces.fill(null);
+
+    boxes.forEach(box => {
+        box.innerText = '';
+        box.style.backgroundColor = '';
+    });
+
+    playerText.innerHTML = 'Game X & O';
+
+    currentPlayer = X_TEXT;
+    gameOver = false; // Reset the game status
+}
+
+startGame();
